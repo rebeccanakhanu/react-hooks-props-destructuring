6914514cbeef8ce0970f74b47d586c5f78c713f3
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-04-27 13:31:03 +0300

    completed assignments

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,13 @@
+// import React from "react";
 import React from "react";
-
 function MovieCard({
   title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
 }) {
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
+      <img src={posterSrc} alt={props.title} />
       <h2>{title}</h2>
       <small>{genres.join(", ")}</small>
     </div>
@@ -15,3 +15,4 @@ function MovieCard({
 }
 
 export default MovieCard;
+
